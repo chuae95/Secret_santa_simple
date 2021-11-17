fbfb16b4f466c2e14257868d28c37a06e2b7fbb2
chuae95 <chua.e95@gmail.com>
2021-11-17 23:45:23 +0800

    Update shift to micro-bot

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const { Telegraf } = require('telegraf');
+// const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf('2116614864:AAH8nGQ1VT32cpUBnTA3gNdy47H-qCssITQ');
+// const bot = new Telegraf('2116614864:AAH8nGQ1VT32cpUBnTA3gNdy47H-qCssITQ');
+
+// bot.command('start', ctx => {
+//     console.log(ctx.from)
+//     bot.telegram.sendMessage(ctx.chat.id, 'Hello there Pizza Slut User', {
+//     })
+// })
+
+// bot.launch();
+
+const { Composer } = require('micro-bot');
+
+const bot = new Composer;
 
 bot.command('start', ctx => {
     console.log(ctx.from)
@@ -8,4 +20,4 @@ bot.command('start', ctx => {
     })
 })
 
-bot.launch();
+module.exports = bot;
